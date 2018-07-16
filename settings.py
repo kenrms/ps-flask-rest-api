511e489ba9f0c102bb5fea81240f43b23f2bf492
Kenneth Ramos <kramos@eduware.com>
2018-07-16 13:29:42 -0400

    Changed settings to use relative path for sqlite db file

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///c:/code/ps-flask-rest-api/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
