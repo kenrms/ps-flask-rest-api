c5a4117517beff6138c21082e45148dbd498c2b5
Kenneth Ramos <kramos@eduware.com>
2018-07-16 10:53:09 -0400

    implemented POST /books with sanitizing request and delivering proper responses

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response, json
 
 app = Flask(__name__)
 
@@ -15,9 +15,34 @@ books = [
     }
 ]
 
+def validBookObject(bookObject):
+    if ("name" in bookObject and "price" in bookObject and "isbn" in bookObject):
+        return True
+    return False
+
 @app.route('/books')
 def get_books():
     return jsonify({'books': books})
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    request_data = request.get_json()
+    if (validBookObject(request_data)):
+        new_book = {
+            "name": request_data['name'],
+            "price":request_data['price'],
+            "isbn": request_data['isbn']
+        }
+        books.insert(0, new_book)
+        response = Response("", 201, mimetype='application/json')
+        response.headers['Location'] = "/books/" + str(new_book['isbn'])
+        return response
+    invalidBookObjectErrorMsg = {
+        "error": "Invalid book object passed in request",
+        "helpString": "Data passed in similar to this {'name': 'bookName', 'price': 8.99, 'isbn': 1234567890}"
+    }
+    response = Response(json.dumps(invalidBookObjectErrorMsg), status=400, mimetype='application/json')
+    return response
 
 @app.route('/books/<int:isbn>')
 def get_book_by_isbn(isbn):
